3247083367f3b64d8b1d9a71a9a73cc98bac5105
wootaiklee <fined000@naver.com>
2024-05-30 23:13:48 +0900

    setting

--- packages/react/components/layout/src/block.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css';
-import { vars } from '@ds/themes'
-export const blockStyle = style({
-	width: '100px',
-	height: '100px',
-	backgroundColor: vars.color.blue[500],
-})
