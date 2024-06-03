6d2e378cdc8795bbbc27c534b14a59b13dbf83a0
wootaiklee <fined000@naver.com>
2024-06-03 23:28:26 +0900

    에러 수정 완료

---
+++ services/storybook/stories/ReactComponents/Layout/Box.stories.tsx
@@ -0,0 +1,21 @@
+import "@ds/react-components-layout/style.css";
+import { Box as _Box } from '@ds/react-components-layout';
+
+export default {
+  title: "React Components/Layout/Box",
+  component: _Box,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const BoxStory = {
+  args: {
+    as: 'button',
+    padding: '5',
+    background: 'pink',
+    boxShadow: 'xl',
+    borderRadius: 'md',
+  }
+};
