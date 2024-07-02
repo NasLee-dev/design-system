970299523535b2a394a742851dbce294f766053d
wootaiklee <fined000@naver.com>
2024-07-02 22:58:40 +0900

    List Items

--- packages/react/components/layout/src/layout/Flex.tsx
+++ packages/react/components/layout/src/layout/Flex.tsx
@@ -1,29 +1,43 @@
 import * as React from "react";
-import { Ref } from "react";
 import { FlexProps } from "./types";
 import { clsx } from "clsx";
 import { BaseStyle, StyleSprinkles } from "../core/style.css";
 import { extractSprinklesProps } from "../utils/properties";
 import { vars } from "@ds/themes";
 
-const Flex = (props: FlexProps, ref: Ref<HTMLElement>) => {
-	const { as = 'div', color, background, align, basis, direction, grow, justify, shrink, wrap, gap, children } = props;
-	return React.createElement(as, { ...props, ref, className: clsx([BaseStyle, StyleSprinkles(extractSprinklesProps(props, Array.from(StyleSprinkles.properties))), props.className]), 
-		style: {
-			display: 'flex',
-			alignItems: align,
-			flexBasis: basis,
-			flexDirection: direction,
-			flexGrow: grow,
-			justifyContent: justify,
-			flexShrink: shrink,
-			flexWrap: wrap,
-			gap: gap,
-			color: color && vars.colors.$scale?.[color]?.[700],
-			background: background && vars.colors.$scale?.[background]?.[100],
-			...props.style,
-		} }, children);
-}
+const Flex = (props: FlexProps, ref: React.Ref<HTMLElement>) => {
+  const { as = "div", color, background, align, basis, direction, grow, justify, shrink, wrap, gap, children } = props;
+
+  return React.createElement(
+    as,
+    {
+      ...props,
+      ref,
+      className: clsx([
+        BaseStyle,
+        StyleSprinkles(
+          extractSprinklesProps(props, Array.from(StyleSprinkles.properties)),
+        ),
+        props.className,
+      ]),
+      style: {
+        display: "flex",
+        alignItems: align,
+        justifyContent: justify,
+        flexDirection: direction,
+        flexWrap: wrap,
+        flexGrow: grow,
+        flexShrink: shrink,
+        flexBasis: basis,
+        gap,
+        color: color && vars.colors.$scale?.[color]?.[700],
+        background: background && vars.colors.$scale?.[background]?.[100],
+        ...props.style,
+      },
+    },
+    children,
+  );
+};
 
 const _Flex = React.forwardRef(Flex);
 export { _Flex as Flex };
