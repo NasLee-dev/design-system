c4d373543b892ec1365ccd12ae1005bbf3fe757f
wootaiklee <fined000@naver.com>
2024-07-19 22:47:03 +0900

    mobile first layout

--- services/page-builder/src/pages/playground.tsx
+++ services/page-builder/src/pages/playground.tsx
@@ -1,10 +1,18 @@
-import { Box } from '@ds/react-components-layout'
+import { vars } from '@ds/themes'
+import Image from 'next/image'
+import { MobileFirstLayout } from '../components/view/MobileFirstLayout'
 
 const PlaygroundPage = () => {
   return (
-    <Box padding={3}>
-      <h1>PlaygroundPage</h1>
-    </Box>
+    <MobileFirstLayout>
+      <h1>{vars.colors.$scale.gray[900]}</h1>
+      <Image 
+        src={`/page_builder_sample_images/1.webp`}
+        alt="Next.js logo"
+        width={200}
+        height={200}
+      />
+    </MobileFirstLayout>
   )
 }
 
