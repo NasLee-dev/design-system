fe957c320c463f4a69a0b6f8bab283f25d8b65cd
wootaiklee <fined000@naver.com>
2025-03-31 22:16:46 +0900

    feat: SearchBar Form, types

--- services/youtube/next.config.js
+++ services/youtube/next.config.js
@@ -2,6 +2,13 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
---
+++ services/youtube/src/shared/components/SearchNavigationBar/index.tsx
@@ -0,0 +1,23 @@
+import * as s from "./style.css";
+import Logo from "./logo.svg";
+import Link from "next/link";
+import { SearchForm } from "./SearchForm";
+import { getMainPageLink } from "@/src/shared/utils/link/page";
+
+export const SearchNavigationBar = () => {
+  return (
+    <nav className={s.navigationBar}>
+      <div className={s.startWrapper}>
+        <Link href={getMainPageLink()}>
+          <div className={s.logo}>
+            <Logo />
+          </div>
+        </Link>
+      </div>
+      <div className={s.centerWrapper}>
+        <SearchForm />
+      </div>
+      <div className={s.endWrapper}></div>
+    </nav>
+  );
+};
