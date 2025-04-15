72194ebff28c3181523964594716fd31bb55a421
wootaiklee <fined000@naver.com>
2025-04-15 21:27:36 +0900

    feat: Search Page Component -> searchParams <> filterItems 연동

---
+++ services/youtube/src/features/search/components/SearchFilter/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { getSearchPageLink } from "@/src/shared/utils/link/page";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { SearchOrder } from "../../api/getSearchVideosList";
+import * as s from "./style.css";
+
+const SEARCH_ORDER_TYPO: Record<SearchOrder, string> = {
+  relevance: "관련순",
+  date: "최신순",
+  viewCount: "조회수 높은 순",
+};
+
+export const SearchFilter = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [order, setOrder] = useState<SearchOrder>(
+    (searchParams.get("order") as SearchOrder) || "relevance",
+  );
+  const filterItems = Object.entries(SEARCH_ORDER_TYPO);
+  const handleClickChip = (order: SearchOrder) => {
+    setOrder(order);
+    router.push(`${getSearchPageLink({ order })}`);
+  };
+  return (
+    <div className={s.container}>
+      <div className={s.wrapper}>
+        {filterItems.map(([key, value]) => (
+          <div
+            key={key}
+            className={s.chip({ active: key === order })}
+            onClick={() => {
+              if (key !== order) {
+                handleClickChip(key as SearchOrder);
+              }
+            }}
+          >
+            {value}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
