725de1e1715883e34af74115a611b202b27e062c
wootaiklee <fined000@naver.com>
2025-05-09 21:25:33 +0900

    feat: ShortsPlayer 추가

--- services/youtube/src/features/videos/shorts/components/ShortsVideoDetail/index.tsx
+++ services/youtube/src/features/videos/shorts/components/ShortsVideoDetail/index.tsx
@@ -1,11 +1,62 @@
 "use client";
+import { Suspense, useState } from "react";
+import { useGetVideosDetail } from "../../../detail/hooks/useGetVideosDetail";
 import { VideoDetailPageParams } from "../../../detail/types";
 import { useHandleInvalidShortsVideoType } from "../../hooks/useHandleInvalidShortsVideoType";
+import { ShortsPlayer } from "../ShortsPlayer";
+import { ButtonItem } from "./ButtonItem";
+import * as s from "./style.css";
+import { AnimatePresence, motion } from "framer-motion";
+import clsx from "clsx";
+import { VideoDetailCommentList } from "../../../detail/components/VideoDetailCommentList";
 
 type Props = VideoDetailPageParams["params"];
 
 export const ShortsVideoDetail = ({ videoId }: Props) => {
+  const {
+    data: { detail: videoDetail },
+  } = useGetVideosDetail({ videoId });
+
   useHandleInvalidShortsVideoType({ videoId });
 
-  return <div>ShortsVideoDetail</div>;
+  const [showComment, setShowComment] = useState(false);
+
+  return (
+    <div className={s.container}>
+      <div className={s.wrapper}>
+        <div className={s.videoWrapper}>
+          <div className={s.buttonWrapper}>
+            <ButtonItem
+              buttonText="댓글"
+              label={videoDetail.commentCountDisplayText}
+              onClick={() => {
+                setShowComment(!showComment);
+              }}
+            />
+          </div>
+          <ShortsPlayer videoId={videoId} autoPlay={true} />
+        </div>
+        <AnimatePresence initial={false}>
+          {showComment && (
+            <motion.div
+              initial="collapsed"
+              exit="collapsed"
+              animate="open"
+              variants={{
+                open: { width: "auto" },
+                collapsed: { width: 0 },
+              }}
+              transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+            >
+              <div className={clsx([s.commentWrapper, "swiper-no-mousewheel"])}>
+                <Suspense>
+                  <VideoDetailCommentList videoId={videoId} />
+                </Suspense>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </div>
+  );
 };
