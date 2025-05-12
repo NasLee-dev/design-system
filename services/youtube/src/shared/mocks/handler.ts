0d266fa6ca4068acecbf5951527d6d7ccb880593
wootaiklee <fined000@naver.com>
2025-05-12 21:40:03 +0900

    feat: ShortsVideosList api 작성 및 호출, useSuspenseInfiniteQuery hook 작성

--- services/youtube/src/shared/mocks/handler.ts
+++ services/youtube/src/shared/mocks/handler.ts
@@ -2,9 +2,11 @@ import { HttpHandler } from "msw";
 import { getMockVideosPopularList } from "@/src/features/main/api/getVideosPopularList.mock";
 import { getMockSearchVideosList } from "@/src/features/search/api/getSearchVideosList.mock";
 import { getMockVideosDetailCommentList } from "@/src/shared/api/youtube/client/videoDetail/getVideoDetailCommentList.mock";
+import { getMockShrotsVideosList } from "@/src/features/videos/shorts/api/getShortsVideosList.mock";
 
 export const handlers: HttpHandler[] = [
   getMockVideosPopularList,
   getMockSearchVideosList,
   getMockVideosDetailCommentList,
+  getMockShrotsVideosList,
 ];
